8f96098e7c0b97cd562bebb19e509b8ca634fb4c
ChrisAlJim <jimenezaot@gmail.com>
2026-02-13 15:19:31 -0500

    Removed imports

--- src/features/expand_cards/Hand.jsx
+++ src/features/expand_cards/Hand.jsx
@@ -1,10 +1,6 @@
 import Card from "./Card";
 import getCards from "./getCards";
 import "./Hand-Card.css";
-import pic1 from '../../assets/IMG_2034.jpg'
-import pic2 from '../../assets/IMG_2110.JPG'
-import pic3 from '../../assets/IMG_2211.JPG'
-import pic4 from '../../assets/WhatsApp Image 2026-01-24 at 01.32.14.jpeg'
 
 const Hand = ({ images }) => {
   const geometry = getCards(images.length)
